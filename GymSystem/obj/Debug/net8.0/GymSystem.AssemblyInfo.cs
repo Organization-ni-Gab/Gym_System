c292a0e297d70cbddee6f27833ec84c1d0c168c3
mikelojeda25 <112379028+mikelojeda25@users.noreply.github.com>
2025-06-15 15:42:23 +0800

    change edit

--- GymSystem/obj/Debug/net8.0/GymSystem.AssemblyInfo.cs
+++ GymSystem/obj/Debug/net8.0/GymSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eea0bf507b26febd192a94b09f3fcb0f7ba90dec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+004c44e7b6406ac645a67b7f3495a268040251e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
